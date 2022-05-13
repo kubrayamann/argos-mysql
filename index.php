b1e5cfd089b9587e3062e23b32c7532a92d0ad88
yamankubra <ymn.kubra@gmail.com>
2022-05-13 22:22:24 +0300

    created table

--- index.php
+++ index.php
@@ -8,8 +8,62 @@
     <title>Document</title>
 </head>
 <body>
+    </br>
     <div class="container">
-        Hello World
+        <div class="row">
+            <h2>Kullanıcı Bilgileri</h2>
+            <form class="form-horizontal" action="/action_page.php">
+            
+            </br>
+            <div class="form-group">        
+                <div class="col-sm-offset-2 col-sm-10">
+                <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+            </div>
+            </form>
+        </div>
+        </br></br>
+        <div class="row">
+            <h2>Eklenen Bilgileri</h2>
+            <table class="table">
+            <thead class="thead-light">
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Ad</th>
+                    <th scope="col">Soyad</th>
+                    <th scope="col">Telefon</th>
+                    <th scope="col">Cinsiyet</th>
+                    <th scope="col">Şehir</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Kübra</td>
+                    <td>Yaman</td>
+                    <td>5551112233</td>
+                    <td>Kadın</td>
+                    <td>İstanbul</td>
+                </tr>
+                <tr>
+                    <th scope="row">2</th>
+                    <td>Kübra</td>
+                    <td>Yaman</td>
+                    <td>5551112233</td>
+                    <td>Kadın</td>
+                    <td>İstanbul</td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td>Kübra</td>
+                    <td>Yaman</td>
+                    <td>5551112233</td>
+                    <td>Kadın</td>
+                    <td>İstanbul</td>
+                </tr>
+            </tbody>
+            </table>
+        </div> 
     </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 </body>
